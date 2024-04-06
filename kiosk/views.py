42cb0471a7a36993607e9ed290970c770b5b184b
kimgibok <ruddjs33@naver.com>
2024-04-06 22:49:32 +0900

    menu update

--- kiosk/views.py
+++ kiosk/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from kiosk.models import Drink
+from kiosk.models import Drink, DrinkType
+
+# class cafe_menuLV(ListView):
+#     model = Drink
+#     template_name = 'kiosk/cafe_menu.html'
+
+#     def get_queryset(self):
+#         # select_related를 사용하여 DrinkType과의 조인을 최적화
+#         return Drink.objects.select_related('type').all()
 
 class cafe_menuLV(ListView):
-    model = Drink
+    model = DrinkType
     template_name = 'kiosk/cafe_menu.html'
-
-    def get_queryset(self):
-        # select_related를 사용하여 DrinkType과의 조인을 최적화
-        return Drink.objects.select_related('type').all()
