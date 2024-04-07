437f53fb8a5df29c9b2122a5e77b3f6ad8bf211f
kimgibok <ruddjs33@naver.com>
2024-04-07 23:39:44 +0900

    menu img

--- kiosk/views.py
+++ kiosk/views.py
@@ -3,13 +3,6 @@ from django.views.generic import ListView
 
 from kiosk.models import Drink, DrinkType
 
-# class cafe_menuLV(ListView):
-#     model = Drink
-#     template_name = 'kiosk/cafe_menu.html'
-
-#     def get_queryset(self):
-#         # select_related를 사용하여 DrinkType과의 조인을 최적화
-#         return Drink.objects.select_related('type').all()
 
 class cafe_menuLV(ListView):
     model = DrinkType
